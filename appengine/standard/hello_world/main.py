4da225804566bdc14d8678905303450abee50a98
shiney28 <shiney28@users.noreply.github.com>
2017-06-08 17:45:13 +0530

    Create main.py

--- appengine/standard/hello_world/main.py
+++ appengine/standard/hello_world/main.py
@@ -19,7 +19,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
-
+        self.response.write('Nice to meet you')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
